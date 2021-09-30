62bcb079b8eda53a7a1559b2bebe3677508131d7
unknown <jiyovalmoria@gmail.com>
2021-09-30 23:09:28 +0800

    Refactored code. Removed get, set.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ namespace CookieClicker
     public interface IBuyShop
     {
         public void BuyMethod();
-        public static int Cost { get; set; }
-        public static int Quant { get; set; }
-        public int amntPerBuy { get; }
 
     }
 
@@ -31,7 +28,7 @@ namespace CookieClicker
     class MiscTexts
     {
         private string choiceText = @$"[0] Stop game || [1] Generate Cookie || [2] Check helper quantity ||
-[3] Check Next Helper Cost || [4] Buy Grandma || [5] Buy Farm ||";
+[3] Check Next Helper Cost || [4] Buy Grandma || [5] Buy Farm || [6] Buy Mines";
         private string errorIntText = "Only integers are accepted on the choices!";
         public string dispChoiceText()
         {
@@ -48,20 +45,12 @@ namespace CookieClicker
 
     class Grandma : IBuyShop, ICheckQuant, ICheckCost
     {
-        string helperName = "Grandma";
+        public string helperName = "Grandma";
         private static int cost = 15;
-        public static int Cost
-        {
-            set { cost = value; }
-            get { return cost; }
-        }
- 
+        private static int costMultiplier = 2;
         private static int quant = 0;
-        public static int Quant
-        {
-            set { quant = value; }
-            get { return quant; }
-        }
+        private static int minCookieGen = 2;
+        public int MinCookieGen { get { return minCookieGen; } }
 
         //amntPerBuy refers to the amount of Grandma you can buy per choice
         public int amntPerBuy { get { return 1; } }
@@ -71,7 +60,7 @@ namespace CookieClicker
             if (currentCookie - cost < 0)
             {
                 int lackingAmnt = cost - currentCookie;
-                
+
                 Console.WriteLine($"You have insufficient " +
                     $"cookies! You need {lackingAmnt} more!");
                 return;
@@ -79,41 +68,77 @@ namespace CookieClicker
             newCookieAmnt = currentCookie - cost;
             CookieData.CurrCke = newCookieAmnt;
             int newQuant = quant + amntPerBuy;
-            int newCost = cost * 2;
-            Cost = newCost;
-            Quant = newQuant;
-            Console.WriteLine($"New cost of {helperName}: " + Cost);
-            Console.WriteLine($"New amount of {helperName}: " + Quant);
+            int newCost = cost * costMultiplier;
+            cost = newCost;
+            quant = newQuant;
+            Console.WriteLine($"New cost of {helperName}: " + cost);
+            Console.WriteLine($"New amount of {helperName}: " + quant);
             Console.WriteLine("New cookie amount: " + CookieData.CurrCke);
 
         }
 
         public int CheckQuant()
         {
-            return Quant;
+            return quant;
         }
         public int CheckCost()
         {
-            return Cost;
+            return cost;
         }
     }
     class Farm : IBuyShop, ICheckQuant, ICheckCost
     {
-        string helperName = "Farm";
+        public string helperName = "Farm";
         private static int cost = 50;
-        public static int Cost
-        {
-            set { cost = value; }
-            get { return cost; }
-        }
-
+        private static int costMultiplier = 2;
         private static int quant = 0;
-        public static int Quant
+        private static int minCookieGen = 10;
+        public int MinCookieGen { get { return minCookieGen; } }
+
+        //amntPerBuy refers to the amount of Grandma you can buy per choice
+        public int amntPerBuy { get { return 1; } }
+        public void BuyMethod()
         {
-            set { quant = value; }
-            get { return quant; }
+            int currentCookie = CookieData.CurrCke;
+            int newCookieAmnt;
+            if (currentCookie - cost < 0)
+            {
+                int lackingAmnt = cost - currentCookie;
+                Console.WriteLine($"You have insufficient " +
+                    $"cookies! You need {lackingAmnt} more!");
+                return;
+            }
+            newCookieAmnt = currentCookie - cost;
+            CookieData.CurrCke = newCookieAmnt;
+            int newQuant = quant + amntPerBuy;
+            int newCost = cost * costMultiplier;
+            cost = newCost;
+            quant = newQuant;
+            Console.WriteLine($"New cost of {helperName}: " + cost);
+            Console.WriteLine($"New amount of {helperName}: " + quant);
+            Console.WriteLine($"New cookie amount: {CookieData.CurrCke}");
+
         }
 
+        public int CheckQuant()
+        {
+            return quant;
+        }
+        public int CheckCost()
+        {
+            return cost;
+        }
+    }
+
+    class Mines : IBuyShop, ICheckQuant, ICheckCost
+    {
+        public string helperName = "Mines";
+        private static int cost = 100;
+        private static int costMultiplier = 5;
+        private static int quant = 0;
+        private static int minCookieGen = 10;
+
+        public int MinCookieGen { get { return minCookieGen; } }
         //amntPerBuy refers to the amount of Grandma you can buy per choice
         public int amntPerBuy { get { return 1; } }
         public void BuyMethod()
@@ -131,21 +156,21 @@ namespace CookieClicker
             CookieData.CurrCke = newCookieAmnt;
             Console.WriteLine($"New cookie amount: {CookieData.CurrCke}");
             int newQuant = quant + amntPerBuy;
-            int newCost = cost * 3;
-            Cost = newCost;
-            Quant = newQuant;
-            Console.WriteLine($"New cost of {helperName}: " + Cost);
-            Console.WriteLine($"New amount of {helperName}: " + Quant);
+            int newCost = cost * costMultiplier;
+            cost = newCost;
+            quant = newQuant;
+            Console.WriteLine($"New cost of {helperName}: " + cost);
+            Console.WriteLine($"New amount of {helperName}: " + quant);
 
         }
 
         public int CheckQuant()
         {
-            return Quant;
+            return quant;
         }
         public int CheckCost()
         {
-            return Cost;
+            return cost;
         }
     }
 
@@ -155,10 +180,13 @@ namespace CookieClicker
         public void getHelperQuant()
         {
             Grandma grma = new Grandma();
-            Console.WriteLine($"Current Grandma amount: {grma.CheckQuant()}");
+            Console.WriteLine($"Current {grma.helperName} amount: {grma.CheckQuant()}");
 
             Farm farm = new Farm();
-            Console.WriteLine($"Current Farm amount: {farm.CheckQuant()}");
+            Console.WriteLine($"Current {farm.helperName} amount: {farm.CheckQuant()}");
+
+            Mines mines = new Mines();
+            Console.WriteLine($"Current {mines.helperName} amount: {mines.CheckQuant()}");
             return;
         }
     }
@@ -168,9 +196,11 @@ namespace CookieClicker
         public void getHelperCost()
         {
             Grandma grma = new Grandma();
-            Console.WriteLine($"Next Grandma cost: {grma.CheckCost()}");
+            Console.WriteLine($"Next {grma.helperName} cost: {grma.CheckCost()}");
             Farm farm = new Farm();
-            Console.WriteLine($"Next Farm cost: {farm.CheckCost()}");
+            Console.WriteLine($"Next {farm.helperName} cost: {farm.CheckCost()}");
+            Mines mines = new Mines();
+            Console.WriteLine($"Next {mines.helperName} cost: {mines.CheckCost()}");
         }
     }
 
@@ -212,8 +242,14 @@ namespace CookieClicker
                 CookieData.CurrCke = newCurrentCookie;
 
             }
+            if (incremMines() >= 1)
+            {
+                newCurrentCookie = incremMines() + newCurrentCookie;
+                CookieData.CurrCke = newCurrentCookie;
+
+            }
             Console.WriteLine($"Current total of Cookie: {CookieData.CurrCke}");
-            Console.WriteLine($"Cookies generated from Helpers: {incremGrandma() + incremFarm()}");
+            Console.WriteLine($"Cookies generated from Helpers: {incremGrandma() + incremFarm() + incremMines()}");
         }
         public int incremGrandma()
         {
@@ -223,7 +259,7 @@ namespace CookieClicker
             {
                 return 0;
             }
-            incremCookie = grma.CheckQuant() * minCookieGen;
+            incremCookie = grma.CheckQuant() * grma.MinCookieGen;
             return incremCookie;
 
         }
@@ -231,16 +267,66 @@ namespace CookieClicker
         {
             Farm farm = new Farm();
             int incremCookie;
-            int minCookieGen = 3;
             if (farm.CheckQuant() <= 0)
             {
                 return 0;
             }
-            incremCookie = farm.CheckQuant() * minCookieGen;
+            incremCookie = farm.CheckQuant() * farm.MinCookieGen;
             return incremCookie;
         }
+        public int incremMines()
+        {
+            Mines mines = new Mines();
+            int incremCookie;
+            if (mines.CheckQuant() <= 0)
+            {
+                return 0;
+            }
+            incremCookie = mines.CheckQuant() * mines.MinCookieGen;
+            return incremCookie;
+        }
+    }
 
+    class CanBuyHelper
+    {
 
+        int currentCookie;
+        
+        public void CheckCanBuy()
+        {
+            canBuyGrandma();
+            canBuyFarm();
+            canBuyMines();
+        }
+        private void canBuyGrandma()
+        {
+            Grandma grma = new Grandma();
+            currentCookie = CookieData.CurrCke;
+            if (grma.CheckCost() <= currentCookie)
+            {
+                Console.WriteLine($"You have enough Cookies to buy a {grma.helperName}!");
+                return;
+            }
+        }
+        private void canBuyFarm()
+        {
+            Farm farm = new Farm();
+            currentCookie = CookieData.CurrCke;
+            if(farm.CheckCost() <= currentCookie){
+                Console.WriteLine($"You have enough Cookies to buy a {farm.helperName}!");
+                return;
+            }
+        }
+        private void canBuyMines()
+        {
+            Mines mines = new Mines();
+            currentCookie = CookieData.CurrCke;
+            if (mines.CheckCost() <= currentCookie)
+            {
+                Console.WriteLine($"You have enough Cookies to buy a {mines.helperName}!");
+                return;
+            }
+        }
     }
 
 
@@ -248,7 +334,7 @@ namespace CookieClicker
     {
         public void CheckChc(int choice, 
             GameStat game_Stat, GenerateCookie gen_Cookie, 
-            Grandma grma, Farm farm, CheckHelperQuant checkHelp_Quant,
+            Grandma grma, Farm farm, Mines mines, CheckHelperQuant checkHelp_Quant,
             CheckHelperCost checkHelp_Cost)
         {
            
@@ -284,6 +370,11 @@ namespace CookieClicker
                         farm.BuyMethod();
                         return;
                     }
+                case 6:
+                    {
+                        mines.BuyMethod();
+                        return;
+                    }
                 default:
                     {
                         Console.WriteLine("Incorrect command!");
@@ -317,6 +408,21 @@ namespace CookieClicker
         }
     }
 
+    class ReachGoal
+    {
+        public void goalReached()
+        {
+            int goalCookieAmnt = 1000000;
+            int currentCookie = CookieData.CurrCke;
+            if (currentCookie >= goalCookieAmnt)
+            {
+                Console.WriteLine("You have reached 1 million cookies! Congratulations!");
+                return;
+            }
+            return;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -326,9 +432,13 @@ namespace CookieClicker
             GenerateCookie gen_Cookie = new GenerateCookie();
             Grandma grma = new Grandma();
             Farm farm = new Farm();
+            Mines mines = new Mines();
             CheckHelperQuant checkHelp_Quant = new CheckHelperQuant();
             CheckHelperCost checkHelp_Cost = new CheckHelperCost();
             MiscTexts miscTexts = new MiscTexts();
+            ReachGoal reachGoal = new ReachGoal();
+            CanBuyHelper canBuyHelper = new CanBuyHelper();
+
 
             game_Stat.StartGame();
 
@@ -341,7 +451,9 @@ namespace CookieClicker
                 {
                     choice = Convert.ToInt32(Console.ReadLine());
                     check_Choice.CheckChc(choice, game_Stat, gen_Cookie, grma,
-                                          farm, checkHelp_Quant, checkHelp_Cost);
+                                          farm, mines, checkHelp_Quant, checkHelp_Cost);
+                    reachGoal.goalReached();
+                    canBuyHelper.CheckCanBuy();
                 }
                 catch
                 {
